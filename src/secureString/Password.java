eb7a510148779c7072bdb77f3998aebb38a9e99a
caasib <107368103+caasib@users.noreply.github.com>
2023-03-10 00:55:10 -0500

    pee

--- src/secureString/Password.java
+++ src/secureString/Password.java
@@ -1,9 +1,11 @@
 package secureString;
+import java.util.Random;
 
 public class Password implements SecureString {
     private String[][] vigTable = new String[70][70];
     private String base = "!@#$%^&*1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private String key = "ie@ZxX#snoq4hU%^!DTp!2*";
+    private Random rand = new Random(10);
 
     public void fillVigTable() {
         for (int i = 0; i < 70; i++) {
@@ -16,8 +18,15 @@ public class Password implements SecureString {
     @Override
     public String digitize(String password) {
         String digital = "";
+        int rNum = rand.nextInt(100);
         for (int i = 0; i < password.length(); i++) {
-            digital += (Math.pow(password.charAt(i), 7) + 2023) / 5;
+            if (rNum <= 50) {
+                digital += (password.charAt(i) + 2023);
+            }
+            else {
+                digital += (password.charAt(i) - 600);
+            }
+            rNum = rand.nextInt(100);
         }
         return digital;
     }
@@ -25,8 +34,15 @@ public class Password implements SecureString {
     @Override
     public String decryptDigitize(String digitalPassword) {
         String original = "";
+        rand.setSeed(10);
+        int rNum = rand.nextInt(100);
         for (int i = 0; i < digitalPassword.length(); i++) {
-            original += Math.pow(((digitalPassword.charAt(i) * 5) - 2023), (double)1/7);
+            if (rNum <= 50) {
+                original += (char)(digitalPassword.charAt(i) - 2023);
+            }
+            else {
+                original += (char)(digitalPassword.charAt(i) + 600);
+            }
         }
         return original;
     }
@@ -36,7 +52,7 @@ public class Password implements SecureString {
         String encrypted = "";
         String editedKey = key.substring(0, password.length());
         int baseIndex, keyIndex;
-        for (int i = password.length(); i > 0; i--) {
+        for (int i = password.length() - 1; i > 0; i--) {
             baseIndex = base.indexOf(password.charAt(i));
             keyIndex = base.indexOf(editedKey.charAt(i));
             encrypted += vigTable[baseIndex][keyIndex];
@@ -55,5 +71,17 @@ public class Password implements SecureString {
             decrypted += vigTable[keyIndex][encryptIndex];
         }
         return decrypted;
+    }
+
+    public static void main(String[] args) {
+        Password pass = new Password();
+        String digit = pass.digitize("apple");
+        String undigit = pass.decryptDigitize(digit);
+        String encrypty = pass.encryptString("apple");
+        String decrypty = pass.decryptString(encrypty);
+        System.out.println(digit);
+        System.out.println(undigit);
+        System.out.println(encrypty);
+        System.out.println(decrypty);
     }
 }
