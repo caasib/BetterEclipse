16b154244df3fb1abb8bdeef03a7bc968587af06
caasib <nketiah2005@gmail.com>
2023-03-22 13:57:22 -0400

    pee

--- src/secureString/Password.java
+++ src/secureString/Password.java
@@ -1,59 +1,51 @@
 package secureString;
+import java.util.*;
 
 public class Password implements SecureString {
-    private String[][] vigTable = new String[70][70];
-    private String base = "!@#$%^&*1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private String key = "ie@ZxX#snoq4hU%^!DTp!2*";
-
-    public void fillVigTable() {
-        for (int i = 0; i < 70; i++) {
-            for (int j = 0; j < 70; j++) {
-                vigTable[i][j] = base.substring(j) + base.substring(0, j);
-            }
-        }
-    }
+    ArrayList<Integer> encryptDigit = new ArrayList<>();
+    int prime = 19;
+    int prm = 2;
+    int base;
+    Random rand = new Random();
 
     @Override
     public String digitize(String password) {
-        String digital = "";
-        for (int i = 0; i < password.length(); i++) {
-            digital += (Math.pow(password.charAt(i), 7) + 2023) / 5;
-        }
-        return digital;
-    }
-
-    @Override
-    public String decryptDigitize(String digitalPassword) {
-        String original = "";
-        for (int i = 0; i < digitalPassword.length(); i++) {
-            original += Math.pow(((digitalPassword.charAt(i) * 5) - 2023), (double)1/7);
-        }
-        return original;
-    }
-
-    @Override
-    public String encryptString(String password) {
         String encrypted = "";
-        String editedKey = key.substring(0, password.length());
-        int baseIndex, keyIndex;
-        for (int i = password.length(); i > 0; i--) {
-            baseIndex = base.indexOf(password.charAt(i));
-            keyIndex = base.indexOf(editedKey.charAt(i));
-            encrypted += vigTable[baseIndex][keyIndex];
+        for (int i = 0; i < password.length(); i++) {
+            int num = (int)Math.pow(prm, password.charAt(i) - 96) % prime;
+            encryptDigit.add(num);
+            encrypted += num;
         }
         return encrypted;
     }
 
     @Override
-    public String decryptString(String encryptedPassword) {
+    public String decryptDigitize(String password) {
         String decrypted = "";
-        String editedKey = key.substring(0, encryptedPassword.length());
-        int keyIndex, encryptIndex;
-        for (int i = 0; i < encryptedPassword.length(); i++) {
-            keyIndex = base.indexOf(editedKey.charAt(i));
-            encryptIndex = base.indexOf(encryptedPassword.charAt(i));
-            decrypted += vigTable[keyIndex][encryptIndex];
+        int randInt = rand.nextInt(prime);
+        for (int i = 0; i < encryptDigit.size(); i++) {
+            int num = (int)Math.pow(encryptDigit.get(i), randInt) % prime;
+            System.out.println(num);
+            decrypted += (char)(num + 96);
         }
         return decrypted;
+    }
+
+    @Override
+    public String encryptString(String password) {
+        return null;
+    }
+
+    @Override
+    public String decryptString(String password) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Password pass = new Password();
+        String bruh = pass.digitize("apple");
+        System.out.println(bruh);
+        System.out.println(pass.encryptDigit);
+        System.out.println(pass.decryptDigitize(bruh));
     }
 }
